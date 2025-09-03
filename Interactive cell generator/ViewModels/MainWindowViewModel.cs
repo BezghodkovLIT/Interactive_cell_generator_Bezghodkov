862b26afb3e1667ea23578028662cf97ae2c0d75
BezghodkovLIT <bezghodkov@dlit.dp.ua>
2025-09-03 23:02:43 +0300

    Перша робота

---
+++ Interactive cell generator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Interactive_cell_generator.Windows;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Interactive_cell_generator.ViewModels
+{
+    partial class MainWindowViewModel : ObservableObject
+    {
+        [RelayCommand]
+        private void OpenFile() 
+        {
+            string[] files = new string[10];
+            files[0] = Path.Combine(AppContext.BaseDirectory, "Content", "Мітохондрія.obj");
+            files[1] = Path.Combine(AppContext.BaseDirectory, "Content", "Сітка і рибосоми.obj");
+            ViewerWindowViewModel viewModel = new ViewerWindowViewModel(files);
+            ViewerWindow viewerWindow = new ViewerWindow();
+            viewerWindow.DataContext = viewModel;
+            viewerWindow.ShowDialog();
+        }
+
+    }
+}
---
+++ Interactive cell generator/ViewModels/ViewerWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using HelixToolkit.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Media3D;
+
+namespace Interactive_cell_generator.ViewModels
+{
+    partial class ViewerWindowViewModel : ObservableObject
+    {
+        public ViewerWindowViewModel( string [] fileNames) 
+        {
+            Random random = new Random();
+            Models = new Model3DGroup();
+            foreach (var file in fileNames) 
+            {               
+                if (file != null) 
+                {
+                    ObjReader reader = new ObjReader();
+                    Model3DGroup value = reader.Read(file);
+                    value.Transform = new TranslateTransform3D()
+                    {
+                        OffsetX = random.Next(0, 100),
+                        OffsetY = random.Next(0, 100),
+                        OffsetZ = random.Next(0, 100)
+                    };
+                    Models.Children.Add(value);
+                }
+            }
+        }
+        [ObservableProperty]
+        private Model3DGroup models;
+    }
+}
